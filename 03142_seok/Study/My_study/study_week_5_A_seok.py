71a966b6b1c303d76c1e7291fa7396407530c1be
hellods0915 <hellods0915@gmail.com>
2025-02-12 23:33:24 +0900

    study_week_5

--- 03142_seok/Study/My_study/study_week_5_A_seok.py
+++ 03142_seok/Study/My_study/study_week_5_A_seok.py
@@ -1,4 +1,4 @@
-'''
+"""
 1. 필요한 함수 분석
 
 가. 뿌요가 4개이상 연결 되어있나 확인하는 함수 
@@ -29,7 +29,7 @@ x \ y =================
 .            .
 11 | [0, 1, 2, 3, 4, 5]
 ========================
-x는 세로(행), y는 가로(열)
+x는 세로 방향으로 센다(행), y는 가로 방향으로 센다(열)
 
 dx = [1, -1, 0, 0]  # 상, 하, 좌, 우 
 dy = [0, 0, -1, 1]
@@ -53,24 +53,21 @@ RRYGG.               00YGG.               .YYGG.               .00GG.           
                                                                                                                                     
 count=0                                   count=1                                   count=2                                   count=3 
 
+"""
 
-
-4. BFS 탐색을 이용한 폭발
-
-
-'''
 from collections import deque
 
 class Puyo:
     
     def __init__(self):
         self.row, self.column = 12, 6
-        self.colors = {"R":1, "G":2, "B":3, "P":4, "Y":5}
         self.delta_x = [1, -1, 0, 0] # 상, 하, 좌, 우
         self.delta_y = [0, 0, -1, 1]
 
-    def insert_puyo_in_board(self):
-        self.board = [list(input().strip()) for _ in range(12)]
+    def insert_puyo_in_board(self, board_data):
+
+        self.board = [list(line.strip()) for line in board_data.strip().splitlines()]
+
 
     def bfs(self, x, y, color, visited):
         queue = deque([(x, y)])
@@ -78,32 +75,70 @@ class Puyo:
         blocks = [(x, y)]        # 연결된 블록 좌표 저장
 
         while queue:
-            current_x, current_y = queue.popleft
+            current_x, current_y = queue.popleft()
             for i in range(4):
                 next_x = current_x + self.delta_x[i]
                 next_y = current_y + self.delta_y[i]
                 if 0 <= next_x < self.row and 0 <= next_y < self.column: # 보드 안에 있으며
-                    if not visited[next_x][next_y]:                      # 방문하지 않았고
+                    if not visited[next_x][next_y]:                      # 아직 방문하지 않았고
                         if self.board[next_x][next_y] == color:          # 색상이 같다면
-                            visited[next_x][next_y] = True
-
-
+                            visited[next_x][next_y] = True               # 방문처리 하고
+                            queue.append((next_x, next_y))
+                            blocks.append((next_x, next_y))
+        return blocks
+    
+    def change_to_dot(self, blocks):
+        for x, y in blocks:
+            self.board[x][y] = '.'
 
     def gravity(self):
-        pass
+        for col in range(self.column):
+            for row in range(self.row -1, -1, -1):       # 아래에서 위로 탐색
+                if self.board[row][col] == '.':         # 빈칸인 '.'을 발견하면
+                    for go_up in range(row - 1, -1, -1): # 그곳의 위쪽에서 블록을 찾은 후 스왑합니다.
+                        if self.board[go_up][col] != '.':
+                            self.board[row][col], self.board[go_up][col] = self.board[go_up][col], self.board[row][col]
+                            break   # 스왑 후 종료
+    
+    def explode_blocks(self):
+        visited = [[False] * self.column for _ in range(self.row)] # 6*12 크기의 False 들을 가지고
+        found = False
 
-    def is_clear(self):
-        pass
-
+        for i in range(self.row):
+            for j in range(self.column):
+                if self.board[i][j] != '.':         # 빈칸인 '.'이 아니면서
+                    if not visited[i][j]:           # 아직 방문하지 않았다면 
+                        blocks = self.bfs(i, j, self.board[i][j], visited)
+                        if len(blocks) >= 4:
+                            self.change_to_dot(blocks)
+                            found = True
+        return found
+    
 if __name__ == "__main__":
+
+    board_data =    """
+                    ......
+                    ......
+                    ......
+                    ......
+                    ......
+                    ......
+                    ......
+                    ......
+                    .Y....
+                    .YG...
+                    RRYG..
+                    RRYGG.
+                    """
+
     game = Puyo()
-    game.insert_puyo_in_board()
+    game.insert_puyo_in_board(board_data)
     cnt = 0
 
-while True:
-    if game.is_clear: # 더 이상 터질 뿌요가 없으면 종료
-        break
-    game.gravity()
-    cnt += 1
+    while True:
+        if not game.explode_blocks(): # 더 이상 터질 뿌요가 없으면 종료
+            break
+        game.gravity()
+        cnt += 1
     
-print(cnt)
+    print(cnt)
