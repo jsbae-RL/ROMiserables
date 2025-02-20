8ac9c0d1eb50ab28a1818869e30075e8e33c021e
chill-guy777 <0802jwl@gmail.com>
2025-02-20 18:42:52 +0900

    6주차

--- study_examples/study_week5_jwlee/study_week5_A_jwlee.py
+++ study_examples/study_week5_jwlee/study_week5_A_jwlee.py
@@ -10,11 +10,12 @@ def solution(answers):
 
 
     for index, A in enumerate(answers):     # answers 요소를 순회하며 pattern과 비교
-        if A == pattern1[index]:
+        if A == pattern1[index % len(pattern1)]:    # if A == pattern1[index]: 문제가 많아질 경우 오류발생가능
+                                                    # [index % len(pattern1)]
             score[0] += 1
-        if A == pattern2[index]:
+        if A == pattern2[index % len(pattern2)]:
             score[1] += 1
-        if A == pattern3[index]:
+        if A == pattern3[index % len(pattern3)]:
             score[2] += 1
 
 #가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return
@@ -23,7 +24,6 @@ def solution(answers):
  
 # 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요
     sorted_win = sorted(winner)
-    
     return sorted_win
 
 answers = [1,2,3,4,5]
